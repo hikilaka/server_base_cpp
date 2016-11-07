e661289882f05ee81922fd494b06abd0f8949442
Zack <zack@sysdevs.org>
2016-11-06 22:01:16 -0600

    added a bunch of different stuff (too long since last commit to remember)

---
+++ include/rsc/util/text_util.hpp
@@ -0,0 +1,15 @@
+#ifndef RSC_UTIL_TEXT_UTIL_HPP
+#define RSC_UTIL_TEXT_UTIL_HPP
+
+#pragma once
+
+#include <string>
+
+namespace rsc {
+    std::uint64_t base37(std::string &text) {
+        
+    }
+}
+
+#endif // RSC_UTIL_TEXT_UTIL_HPP
+
